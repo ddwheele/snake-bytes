f3441a4f0748bcb15a1bfae7e680e7b91a77cd56
DW Wheeler <dawn.wheeler@gmail.com>
2024-11-29 21:13:26 -0800

    may almost be working now

--- ShortestPath/ShortestPath.py
+++ ShortestPath/ShortestPath.py
@@ -6,6 +6,9 @@ import yaml
 from queue import PriorityQueue
 
 MAX_FLOAT = sys.float_info.max
+MAP = {}
+X_SPACING = 1
+Y_SPACING = 1
 
 class Node:
     # dist is distance to start node
@@ -16,42 +19,56 @@ class Node:
         self.parent = parent
         self.distance = dist
 
+    def print_me_grid(self):
+        print("---")
+        print(f"Node grid coords: {self.point}")
+        print("my distance is " + str(self.distance))
+        print("---")
+       
+    def print_me_real(self):
+        xy = grid_to_real(self.point)
+        print(f"Node real coords: {xy}")
+        print("my distance is " + str(self.distance))
+
 # return true if map square is available, false otherwise
-def grid_square_fair(map, ij):
+def grid_square_fair(ij):
     i = ij[0]
     j = ij[1]
-    size = np.shape(map)
+    size = np.shape(MAP)
     max_i = size[0]
     max_j = size[1]
     if i < 0 or j < 0  or  i > max_i or j > max_j:
+        print(f"{ij} is out of bounds")
         return False
 
-    val = map[i][j]
+    val = MAP[i][j]
     if val > 0:
-        return True
-    else:
+        print(f"{ij} is occupied")
         return False
+    else:
+        print(f"{ij} is clear")
+        return True
 
 
-def real_to_grid(xy, x_spacing, y_spacing):
-    j = xy[0]/x_spacing - 0.5
-    i = xy[1]/y_spacing - 0.5
+def real_to_grid(xy):
+    j = xy[0]/X_SPACING - 0.5
+    i = xy[1]/Y_SPACING - 0.5
   #  print("i=" + str(i) + ", j=" + str(j))
     return (i, j)
 
-def grid_to_real(ij, x_spacing, y_spacing):
-    x = (ij[1] + 0.5)*x_spacing
-    y = (ij[0] + 0.5)*y_spacing
+def grid_to_real(ij):
+    x = (ij[1] + 0.5)*X_SPACING
+    y = (ij[0] + 0.5)*Y_SPACING
     return (x, y)
 
-def find_nearest_grid(xy, x_spacing,y_spacing):
-    ij = real_to_grid(xy, x_spacing,y_spacing)
+def find_nearest_grid(xy):
+    ij = real_to_grid(xy)
 
     near_i = round(ij[0][0])
     near_j = round(ij[1][0])
     return (near_i, near_j)
 
-def dijkstras(occupancy_map,x_spacing,y_spacing,start,goal):
+def dijkstras(start,goal):
     """
     Implements Dijkstra's shortest path algorithm
     Input:
@@ -68,10 +85,10 @@ def dijkstras(occupancy_map,x_spacing,y_spacing,start,goal):
         metric coordinates)
     """
     ## Find the nearest node to the start node
-    start_coord = find_nearest_grid(start, x_spacing, y_spacing)
+    start_coord = find_nearest_grid(start)
 
     ## Find the nearest node to the end node
-    goal_coord = find_nearest_grid(goal, x_spacing, y_spacing)
+    goal_coord = find_nearest_grid(goal)
 
     print("start is near to " + str(start_coord[0]) + ", " + str(start_coord[1]))
     print("goal is near to " + str(goal_coord[0]) + ", " + str(goal_coord[1]))
@@ -82,24 +99,37 @@ def dijkstras(occupancy_map,x_spacing,y_spacing,start,goal):
     # print("checking_start: " + str(checking_start[0]) + ", " + str(checking_start[1]))
     # print("checking_goal: " + str(checking_goal[0]) + ", " + str(checking_goal[1]))
 
-    start_node = Node(start_coord)
-    goal_node = Node(goal_coord)
+    # dictionary with key coordinate tuple and value Node
+    node_dict = {}
+    # premake all the dictionary entries
+    size = np.shape(MAP)
+    max_i = size[0]
+    max_j = size[1]
+    for j in range(0, max_j):
+        for i in range(0, max_i):
+            node_dict[(i,j)] = Node((i,j))
+
+    start_node = node_dict[start_coord]
+    start_node.distance = 0
+    start_node.print_me_grid()
+    goal_node = node_dict[goal_coord]
+
     # priority queue linking distance from start to tuple with coordinates
     node_q = PriorityQueue()
-    node_q.put(0, start_coord)
+    node_q.put((0, start_coord))
+   # node_dict[goal_coord] = goal_node
 
-    # dictionary with key coordinate tuple and value Node
-    node_dict = {start_coord:start_node}
-    node_dict[goal_coord] = goal_node
-
-    best_coords = node_q.get()
-    while(best_coords != goal_coord):
+    best_q_entry = node_q.get()
+    curr_coords = best_q_entry[1]
+    print(f"THE START is {curr_coords}")
+    while(curr_coords != goal_coord):
         # Take the node with the smallest distance
-        best_coords = node_q.get()
-        curr_i = best_coords[0]
-        curr_j = best_coords[1]
-        current_node = node_dict[best_coords]
+        curr_i = curr_coords[0]
+        curr_j = curr_coords[1]
+        current_node = node_dict[curr_coords]
+        
         current_node_dist = current_node.distance
+        print("current_node_dist = " + str(current_node_dist))
         
         # find the neighbors of current_node
         north_coord = (curr_i-1, curr_j)
@@ -108,20 +138,41 @@ def dijkstras(occupancy_map,x_spacing,y_spacing,start,goal):
         west_coord = (curr_i, curr_j+1)
 
         # Visit all of its neighbors and add them with the distance to the q
-        node_q = evaluate_this_neighbor(occupancy_map, node_q, node_dict, current_node, north_coord,current_node_dist)
-        node_q = evaluate_this_neighbor(occupancy_map, node_q, node_dict, current_node, south_coord,current_node_dist)
-        node_q = evaluate_this_neighbor(occupancy_map, node_q, node_dict, current_node, east_coord, current_node_dist)
-        node_q = evaluate_this_neighbor(occupancy_map, node_q, node_dict, current_node, west_coord, current_node_dist)
+        node_q = evaluate_this_neighbor(node_q, node_dict, current_node, north_coord,current_node_dist)
+        print("Contents of the PriorityQueue:", list(node_q.queue))
+        node_q = evaluate_this_neighbor(node_q, node_dict, current_node, south_coord,current_node_dist)
+        print("Contents of the PriorityQueue:", list(node_q.queue))
+        node_q = evaluate_this_neighbor(node_q, node_dict, current_node, east_coord, current_node_dist)
+        print("Contents of the PriorityQueue:", list(node_q.queue))
+        node_q = evaluate_this_neighbor(node_q, node_dict, current_node, west_coord, current_node_dist)
+        print("Contents of the PriorityQueue:", list(node_q.queue))
+
+        best_q_entry = node_q.get()
+        curr_coords = best_q_entry[1]
+
+        print(f"NEXT TO EXPAND is {curr_coords}")
+
+        input("Press Enter to continue...")
 
 
-def evaluate_this_neighbor(map, node_q, node_dict, current_node, neighbor_coord, curr_dist):
+def evaluate_this_neighbor(node_q, node_dict, current_node, neighbor_coord, curr_dist):
+    print(f"neighbor_coord is {neighbor_coord}")
+    if not grid_square_fair(neighbor_coord):
+        print(f"rejecting {neighbor_coord}")
+        return node_q
+   # print("=====")
+    print(f"KEEPING {neighbor_coord}")
     # get neighbor
     neighbor_node = node_dict[neighbor_coord]
+
     # update its distance and parent
     neighbor_node.parent = current_node
     neighbor_node.distance = curr_dist+1
+
+    neighbor_node.print_me_grid()
+    
     # add it to the priority queue with new distance
-    node_q.put(curr_dist+1, neighbor_coord)
+    node_q.put((curr_dist+1, neighbor_coord))
     # return the modified queue
     return node_q
 
@@ -239,6 +290,10 @@ def test_for_grader():
 
 
 def main():
+    global MAP
+    global X_SPACING
+    global Y_SPACING
+
     # Load parameters from yaml
     param_path = 'params.yaml' # rospy.get_param("~param_path")
     f = open(param_path,'r')
@@ -246,18 +301,21 @@ def main():
     f.close()
     params = yaml.load(params_raw, Loader=yaml.Loader)
     # Get params we need
-    occupancy_map = np.array(params['occupancy_map'])
+   # occupancy_map = np.array(params['occupancy_map'])
     pos_init = np.array(params['pos_init'])
     pos_goal = np.array(params['pos_goal'])
 
     print("pos_init is " + str(pos_init[0]) + ", " + str(pos_init[1]))
     print("pos_goal is " + str(pos_goal[0]) + ", " + str(pos_goal[1]))
-    x_spacing = params['x_spacing']
-    y_spacing = params['y_spacing']
+    #x_spacing = params['x_spacing']
+    #y_spacing = params['y_spacing']
+    X_SPACING = params['x_spacing']
+    Y_SPACING = params['y_spacing']
+    MAP = np.array(params['occupancy_map'])
 
-    print("x_spacing is " + str(x_spacing))
-    print("y_spacing is " + str(y_spacing))
-    path = dijkstras(occupancy_map,x_spacing,y_spacing,pos_init,pos_goal)
+   # print("x_spacing is " + str(x_spacing))
+   # print("y_spacing is " + str(y_spacing))
+    path = dijkstras(pos_init,pos_goal)
     print(path)
 
 if __name__ == '__main__':
